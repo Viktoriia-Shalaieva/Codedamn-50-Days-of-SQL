df09db82fd2c98f1365fe057133d98dc7bcfa868
Viktoriia Shalaieva <163899438+Viktoriia-Shalaieva@users.noreply.github.com>
2024-10-08 15:05:57 +0300

    Create solution.sql

---
+++ solution.sql
@@ -0,0 +1,101 @@
+/*
+Filtering Patients by Weight Range in SQLite
+Your challenge is to construct an SQL query to achieve the following:
+
+Retrieve the first_name and last_name of patients table.
+The query should only select patients whose weight is within the range of 100 to 120 (inclusive).
+Table Schema
+You have to make use of the patients table. Here is the complete schema of the table for your reference
+
+patient_id
+first_name
+last_name
+gender
+birth_date
+city
+province_id
+allergies
+height
+weight
+Key Concepts
+SQLite: A C-language library that provides a lightweight disk-based database. It doesn’t require a separate server process.
+SQL SELECT Statement: Used to select data from a database and fetch the result table. The result can be stored in a table or used as it is.
+SQL WHERE Clause: Used to filter the results and extract only the necessary records.
+BETWEEN Operator: In SQL, the BETWEEN operator is used to filter the result set based on a range. It can be used with numbers, text and even dates.
+*/
+select 
+    first_name
+    , last_name
+from patients
+where weight between 100 and 120
+;
+
+/*
+Finding Patients from Hamilton with Allergies Using SQL
+Write an SQL query to find a list of patients' first_name, last_name, and allergies who are from the city of Hamilton and have non-null values in the allergies column.
+
+Tables Used: patients
+Important Concepts
+SQL Keywords: Understanding SQL keywords like SELECT, FROM, and WHERE will be essential.
+NULL Values: Know how to handle NULL values in SQL, especially when you want to include or exclude them in your results.
+*/
+SELECT 
+    first_name
+    , last_name
+    , allergies
+FROM patients    
+WHERE  city = 'Hamilton' 
+    AND allergies IS NOT NULL
+;
+
+/*
+Filtering and Sorting Customer Data by City and Contact Name in SQLite
+In this lab, you will be working with the SQLite database northwind.sqlite. Your main task is to construct an SQL query to retrieve specific information from the customers table.
+
+Objective
+Your SQL query should fulfill the following requirements:
+
+Retrieve the city, company_name, and contact_name columns from the customers table.
+Filter the records to only include rows where the city name contains the letter 'L'.
+Sort the resulting data set by contact_name in ascending order.
+Key Concepts
+To complete this lab, you should be familiar with the following SQL operations:
+
+SELECT for specifying the columns you want to retrieve
+WHERE for filtering rows based on a condition
+ORDER BY for sorting the results
+*/
+SELECT city, company_name, contact_name
+FROM customers
+WHERE city LIKE '%L%'
+ORDER BY contact_name ASC
+;
+
+/*
+Finding the Tallest Patient's Name and Height
+Your goal is to retrieve specific information about patients based on certain criteria. Specifically, you are tasked with the following:
+
+Retrieve the first_name, last_name, and height of the patient with the greatest height from the patients table
+Schema
+Schema of the patients table for reference
+
+patient_id
+first_name
+last_name
+gender
+birth_date
+city
+province_id
+allergies
+height
+weight
+Concepts:
+SELECT Statement: Used to select data from a database. The result is then stored in a result table.
+Aggregate Functions: SQL aggregate functions return a single value, calculated from values in a column. The function MAX() is one such function which returns the maximum value of the selected column.
+*/
+
+SELECT first_name
+    , last_name
+    , MAX(height) AS height
+FROM patients
+;
