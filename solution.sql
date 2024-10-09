0ba8ffa1740863a53f4c764b40d288169dea1682
Viktoriia Shalaieva <163899438+Viktoriia-Shalaieva@users.noreply.github.com>
2024-10-09 21:30:42 +0300

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -99,3 +99,38 @@ SELECT first_name
     , MAX(height) AS height
 FROM patients
 ;
+
+/*
+Filtering Patients by Specific IDs in a Hospital Database
+Easy
+128
+1
+93.9% Acceptance
+The database focuses on hospital data, and for this particular exercise you'll be using the patients table.
+
+Schema
+patients table schema
+
+patient_id
+first_name
+last_name
+gender
+birth_date
+city
+province_id
+allergies
+height
+weight
+Task
+Write a SQL query to show all columns for patients who have specific patient_ids: 1, 45, 534, 879, 1000.
+
+Concepts to Know
+SELECT: To specify the columns you want to retrieve.
+WHERE: To filter the records based on certain conditions.
+IN: To specify multiple values in a WHERE clause.
+*/
+
+SELECT *
+FROM patients
+WHERE patient_id IN (1, 45, 534, 879, 1000)
+;
