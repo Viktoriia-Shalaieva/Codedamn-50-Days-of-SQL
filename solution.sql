8fdda5d42710cbccb90aed5569853fe44270d56a
Viktoriia Shalaieva <163899438+Viktoriia-Shalaieva@users.noreply.github.com>
2024-10-13 22:10:51 +0300

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -230,6 +230,7 @@ ORDER BY FirstName ASC
 ;    
 
 /*
+DAY 8
 Identifying Same-Day Hospital Admissions and Discharges
 Your task is to write a SQL query to show all the columns from the admissions table where the patient was admitted and discharged on the same day.
 
@@ -245,4 +246,51 @@ FROM
     admissions
 WHERE 
     admission_date = discharge_date
+;
+
+/*
+DAY 9
+Distinct city names (chinook database)
+In this exercise, you'll be working with the chinook database. Your task is to solve a series of challenges that test your SQL skills.
+
+Lab Dataset
+You'll be using the chinook database, which represents a digital music shop. Here's a quick breakdown of the tables and the key columns:
+
+Artist: "ArtistId", "Name"
+Album: "AlbumId", "Title", "ArtistId"
+Track: "TrackId", "Name", "AlbumId", "MediaTypeId", "GenreId", "Composer", "Milliseconds", "Bytes", "UnitPrice"
+MediaType: "MediaTypeId", "Name"
+Genre: "GenreId", "Name"
+Playlist: "PlaylistId", "Name"
+PlaylistTrack: "PlaylistId", "TrackId"
+Invoice: "InvoiceId", "CustomerId", "InvoiceDate", "BillingAddress", "BillingCity", "BillingState", "BillingCountry", "BillingPostalCode", "Total"
+InvoiceLine: "InvoiceLineId", "InvoiceId", "TrackId", "UnitPrice", "Quantity"
+Customer: "CustomerId", "FirstName", "LastName", "Company", "Address", "City", "State", "Country", "PostalCode", "Email", "SupportRepId"
+Employee: "EmployeeId", "LastName", "FirstName", "Title", "ReportsTo", "BirthDate", "HireDate", "Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email"
+Instructions
+Writing SQL Queries: Write SQL queries to solve the challenges. Make sure to name all the columns exactly as mentioned in the challenge statement.
+Using Double Quotes: Whenever referencing column names or table names, use double quotes for clarity, e.g., SELECT "Name" FROM "Artist".
+Order Matters: Always ensure that the ordering specified in the challenge is strictly followed. This is important to make sure your solution matches the expected output, as the evaluation may involve direct comparison of rows.
+Be Precise: Ensure that the queries you write follow the exact specifications of the challenges. Avoid adding or removing columns that are not explicitly mentioned.
+When you're ready to submit, ensure your SQL query matches the problem statement's requirements, and remember to test your solution to ensure accuracy!
+
+Good luck, and happy querying!
+
+Challenges Information
+Challenge 1: Distinct Customer Cities
+Objective: Fetch a list of all the unique cities in which the customers live.
+
+Details:
+
+Dataset: Use the Customer table from the chinook database.
+Columns to Display: Display the City column.
+Ordering: Results should be ordered alphabetically by the city name.
+Uniqueness: Ensure that each city name appears only once in the result.
+*/
+SELECT 
+    DISTINCT "City" 
+FROM 
+    "Customer"
+ORDER BY 
+    "CITY"
 ;
