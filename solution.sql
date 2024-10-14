90a891692004d0691947c34565ba1728bb50d432
Viktoriia Shalaieva <163899438+Viktoriia-Shalaieva@users.noreply.github.com>
2024-10-14 17:06:32 +0300

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -294,3 +294,20 @@ FROM
 ORDER BY 
     "CITY"
 ;
+/*
+DAY 10
+Retrieve Even-Numbered Order IDs from the Orders Table
+
+Retrieve all the even-numbered Order_id from the orders table.
+
+Concepts to Review:
+SELECT Statement: The fundamental SQL command used to retrieve data from a table.
+WHERE Clause: Used in SQL to filter records based on specific conditions.
+MOD Function: In SQL, the MOD function returns the remainder of a division. For example, MOD(column_name, n) will give the remainder when column_name is divided by n.
+*/
+SELECT 
+    Order_id
+FROM orders
+WHERE 
+    MOD(Order_id, 2) = 0
+;
