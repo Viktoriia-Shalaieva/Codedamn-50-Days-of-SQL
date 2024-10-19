dc216b5b8c204b26e038187ef999ea96fa738d9f
Viktoriia Shalaieva <163899438+Viktoriia-Shalaieva@users.noreply.github.com>
2024-10-19 20:18:20 +0300

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -476,3 +476,26 @@ GROUP BY
 HAVING 
     COUNT(*) >= 2
 ;
+
+/*
+DAY 15
+Identifying Patients with Multiple Admissions for the Same Diagnosis
+
+In this lab, you are presented with a database that contains hospital admission records. Your task is to write an SQL query to find patients who have been admitted multiple times for the same diagnosis. You are required to display the patient_id and diagnosis for these cases.
+
+Important Concepts
+GROUP BY: Useful for aggregating data based on one or more columns.
+HAVING: Allows you to filter records after performing an aggregation.
+COUNT(): SQL function for counting the number of rows in a result set.
+*/
+SELECT
+    patient_id
+    , diagnosis
+FROM 
+    admissions
+GROUP BY 
+    patient_id
+    , diagnosis
+HAVING 
+    COUNT(*) >= 2
+;
