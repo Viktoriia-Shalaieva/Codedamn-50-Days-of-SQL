54bd24438562320b2229a8782a112d117d0ffc7a
Viktoriia Shalaieva <163899438+Viktoriia-Shalaieva@users.noreply.github.com>
2024-10-22 20:36:13 +0300

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -547,3 +547,36 @@ FROM
 WHERE 
     last_name = 'Maroni'
 ; 
+
+/*
+DAY 18
+Filtering Customers by Country in Northwind Database
+
+Retrieve the following columns: contact_name, address, and city from the table customers
+The query should only include customers that are NOT from the countries Germany, Mexico, or Spain.
+customers table schema
+customer_id
+company_name
+contact_name
+contact_title
+address
+city
+region
+postal_code
+country
+phone
+fax
+Important Concepts
+SQL SELECT statement: Used to specify the columns that should be returned in the result set.
+SQL WHERE clause: Used for filtering records based on specific conditions.
+SQL NOT IN clause: Used to exclude records that match values in a list.
+*/
+SELECT
+    contact_name
+    , address
+    , city
+FROM 
+    customers
+WHERE 
+    country NOT IN ('Germany', 'Mexico', 'Spain')
+;
